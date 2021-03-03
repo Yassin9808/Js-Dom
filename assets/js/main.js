c0854c916c0fff43d9e0b58a8a24e062e40d0c1a
Yassin <yassin.ntumba@gmail.com>
2021-03-03 09:48:42 +0100

    1.1

---
+++ assets/js/main.js
@@ -0,0 +1,19 @@
+let h1 = document.getElementsByTagName('h1')
+console.log(h1);
+// exo2
+let premierH3 = document.getElementsByTagName('H3')[0]
+let secondH3 = document.getElementsByTagName('H3')[1] 
+let dernierH3 = document.getElementsByTagName('H3')[2]
+console.log(premierH3, secondH3, dernierH3);
+// let everyH3 = document.getElementsByTagName('H3')
+// everyH3.forEach(element=> {
+//     console.log(element );
+// });
+// exo 3
+let p = document.getElementsByTagName('p')
+let p2 = document.getElementsByTagName('p')[1]
+console.log(p2);
+// exo4
+let li = document.getElementsByTagName('li')
+let li3 = document.getElementsByTagName('li')[2]
+console.log(li3);
